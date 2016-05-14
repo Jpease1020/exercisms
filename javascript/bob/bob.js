f55bee65d6f1ef867b2e21d644bae2ae6d88dc37
Justin Pease <justinpease2@gmail.com>
2016-05-14 11:52:42 -0600

    adds bob js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,13 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-
+  if(input == input.toUpperCase()){
+    return 'Whoa, chill out!'
+  }
+  else if(input.endsWith('?')){
+    return 'Sure.'
+  }
+  else { return 'Whatever.' }
 };
 
 module.exports = Bob;
